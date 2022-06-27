aab9be2e106e5311cb91a8f88494e5db07a4f8be
Alan Jo K <alanjok7@gmail.com>
2022-06-27 21:01:08 +0530

    Add files via upload

--- traintest.py
+++ traintest.py
@@ -43,13 +43,14 @@ plt.show()
 
 df = data[['PrefixSuffix-', 'SubDomains', 'HTTPS','AnchorURL','WebsiteTraffic','class']]
 sns.pairplot(data = df,hue="class",corner=True)
+plt.show()
 
 # Phishing Count in pie chart
-
+"""
 data['class'].value_counts().plot(kind='pie',autopct='%1.2f%%')
 plt.title("Phishing Count")
 plt.show()
-
+"""
 #Splitting the dataset into dependant and independant features
 
 X = data.drop(["class"],axis =1)
@@ -62,6 +63,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 X_train.shape, y_train.shape, X_test.shape, y_test.shape
 
 #Model Building & Training
+
 #Creating holders to store the model performance results
 
 ML_Model = []
@@ -78,6 +80,194 @@ def storeResults(model, a,b,c,d):
     f1_score.append(round(b, 3))
     recall.append(round(c, 3))
     precision.append(round(d, 3))
+
+
+
+# K-Nearest Neighbors Classifier model
+
+from sklearn.neighbors import KNeighborsClassifier
+
+# instantiate the model
+knn = KNeighborsClassifier(n_neighbors=1)
+
+# fit the model 
+knn.fit(X_train,y_train)
+
+#predicting the target value from the model for the samples
+y_train_knn = knn.predict(X_train)
+y_test_knn = knn.predict(X_test)
+
+#computing the accuracy,f1_score,Recall,precision of the model performance
+
+acc_train_knn = metrics.accuracy_score(y_train,y_train_knn)
+acc_test_knn = metrics.accuracy_score(y_test,y_test_knn)
+print("K-Nearest Neighbors : Accuracy on training Data: {:.3f}".format(acc_train_knn))
+print("K-Nearest Neighbors : Accuracy on test Data: {:.3f}".format(acc_test_knn))
+print()
+
+f1_score_train_knn = metrics.f1_score(y_train,y_train_knn)
+f1_score_test_knn = metrics.f1_score(y_test,y_test_knn)
+print("K-Nearest Neighbors : f1_score on training Data: {:.3f}".format(f1_score_train_knn))
+print("K-Nearest Neighbors : f1_score on test Data: {:.3f}".format(f1_score_test_knn))
+print()
+
+recall_score_train_knn = metrics.recall_score(y_train,y_train_knn)
+recall_score_test_knn = metrics.recall_score(y_test,y_test_knn)
+print("K-Nearest Neighborsn : Recall on training Data: {:.3f}".format(recall_score_train_knn))
+print("Logistic Regression : Recall on test Data: {:.3f}".format(recall_score_test_knn))
+print()
+
+precision_score_train_knn = metrics.precision_score(y_train,y_train_knn)
+precision_score_test_knn = metrics.precision_score(y_test,y_test_knn)
+print("K-Nearest Neighbors : precision on training Data: {:.3f}".format(precision_score_train_knn))
+print("K-Nearest Neighbors : precision on test Data: {:.3f}".format(precision_score_test_knn))
+
+#computing the classification report of the model
+
+print(metrics.classification_report(y_test, y_test_knn))
+
+training_accuracy = []
+test_accuracy = []
+# try max_depth from 1 to 20
+depth = range(1,20)
+for n in depth:
+    knn = KNeighborsClassifier(n_neighbors=n)
+    knn.fit(X_train, y_train)
+    # record training set accuracy
+    training_accuracy.append(knn.score(X_train, y_train))
+    # record generalization accuracy
+    test_accuracy.append(knn.score(X_test, y_test))
+    
+
+#plotting the training & testing accuracy for n_estimators from 1 to 20
+plt.figure(figsize=None)
+plt.plot(depth, training_accuracy, label="training accuracy")
+plt.plot(depth, test_accuracy, label="test accuracy")
+plt.title("K-NN Classifier")
+plt.ylabel("Accuracy") 
+plt.xlabel("n_neighbors")
+plt.legend()
+plt.show()
+
+
+
+# Support Vector Classifier model 
+
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
+# defining parameter range
+param_grid = {'gamma': [0.1],'kernel': ['rbf','linear']}
+
+svc = GridSearchCV(SVC(), param_grid)
+
+# fitting the model for grid search
+svc.fit(X_train, y_train)
+
+#predicting the target value from the model for the samples
+y_train_svc = svc.predict(X_train)
+y_test_svc = svc.predict(X_test)
+
+#computing the accuracy, f1_score, Recall, precision of the model performance
+
+acc_train_svc = metrics.accuracy_score(y_train,y_train_svc)
+acc_test_svc = metrics.accuracy_score(y_test,y_test_svc)
+print("Support Vector Machine : Accuracy on training Data: {:.3f}".format(acc_train_svc))
+print("Support Vector Machine : Accuracy on test Data: {:.3f}".format(acc_test_svc))
+print()
+
+f1_score_train_svc = metrics.f1_score(y_train,y_train_svc)
+f1_score_test_svc = metrics.f1_score(y_test,y_test_svc)
+print("Support Vector Machine : f1_score on training Data: {:.3f}".format(f1_score_train_svc))
+print("Support Vector Machine : f1_score on test Data: {:.3f}".format(f1_score_test_svc))
+print()
+
+recall_score_train_svc = metrics.recall_score(y_train,y_train_svc)
+recall_score_test_svc = metrics.recall_score(y_test,y_test_svc)
+print("Support Vector Machine : Recall on training Data: {:.3f}".format(recall_score_train_svc))
+print("Support Vector Machine : Recall on test Data: {:.3f}".format(recall_score_test_svc))
+print()
+
+precision_score_train_svc = metrics.precision_score(y_train,y_train_svc)
+precision_score_test_svc = metrics.precision_score(y_test,y_test_svc)
+print("Support Vector Machine : precision on training Data: {:.3f}".format(precision_score_train_svc))
+print("Support Vector Machine : precision on test Data: {:.3f}".format(precision_score_test_svc))
+
+#computing the classification report of the model
+
+print(metrics.classification_report(y_test, y_test_svc))
+
+
+
+# Decision Tree Classifier model 
+
+from sklearn.tree import DecisionTreeClassifier
+
+# instantiate the model 
+tree = DecisionTreeClassifier(max_depth=30)
+
+# fit the model 
+tree.fit(X_train, y_train)
+
+#predicting the target value from the model for the samples
+
+y_train_tree = tree.predict(X_train)
+y_test_tree = tree.predict(X_test)
+
+#computing the accuracy, f1_score, Recall, precision of the model performance
+
+acc_train_tree = metrics.accuracy_score(y_train,y_train_tree)
+acc_test_tree = metrics.accuracy_score(y_test,y_test_tree)
+print("Decision Tree : Accuracy on training Data: {:.3f}".format(acc_train_tree))
+print("Decision Tree : Accuracy on test Data: {:.3f}".format(acc_test_tree))
+print()
+
+f1_score_train_tree = metrics.f1_score(y_train,y_train_tree)
+f1_score_test_tree = metrics.f1_score(y_test,y_test_tree)
+print("Decision Tree : f1_score on training Data: {:.3f}".format(f1_score_train_tree))
+print("Decision Tree : f1_score on test Data: {:.3f}".format(f1_score_test_tree))
+print()
+
+recall_score_train_tree = metrics.recall_score(y_train,y_train_tree)
+recall_score_test_tree = metrics.recall_score(y_test,y_test_tree)
+print("Decision Tree : Recall on training Data: {:.3f}".format(recall_score_train_tree))
+print("Decision Tree : Recall on test Data: {:.3f}".format(recall_score_test_tree))
+print()
+
+precision_score_train_tree = metrics.precision_score(y_train,y_train_tree)
+precision_score_test_tree = metrics.precision_score(y_test,y_test_tree)
+print("Decision Tree : precision on training Data: {:.3f}".format(precision_score_train_tree))
+print("Decision Tree : precision on test Data: {:.3f}".format(precision_score_test_tree))
+
+#computing the classification report of the model
+
+print(metrics.classification_report(y_test, y_test_tree))
+
+training_accuracy = []
+test_accuracy = []
+# try max_depth from 1 to 30
+depth = range(1,30)
+for n in depth:
+    tree_test = DecisionTreeClassifier(max_depth=n)
+    tree_test.fit(X_train, y_train)
+    # record training set accuracy
+    training_accuracy.append(tree_test.score(X_train, y_train))
+    # record generalization accuracy
+    test_accuracy.append(tree_test.score(X_test, y_test))
+    
+
+#plotting the training & testing accuracy for max_depth from 1 to 30
+
+#plt.figure(figsize=None)
+plt.plot(depth, training_accuracy, label="training accuracy")
+plt.plot(depth, test_accuracy, label="test accuracy")
+plt.title("Decision Tree Classifier")
+plt.ylabel("Accuracy")  
+plt.xlabel("max_depth")
+plt.legend()
+plt.show()
+
+
 
 #Gradient Boosting Classifier Model
 
@@ -142,6 +332,7 @@ for n in depth:
 plt.figure(figsize=None)
 plt.plot(depth,training_accuracy,label="training accuracy")
 plt.plot(depth,test_accuracy,label="test accuracy")
+plt.title("Gradient Boosting Classifier")
 plt.ylabel("Accuracy")  
 plt.xlabel("learning_rate")
 plt.legend()
@@ -166,9 +357,12 @@ for n in depth:
 plt.figure(figsize=None)
 plt.plot(depth,training_accuracy,label="training accuracy")
 plt.plot(depth,test_accuracy,label="test accuracy")
+plt.title("Gradient Boosting Classifier")
 plt.ylabel("Accuracy")  
 plt.xlabel("max_depth")
 plt.legend()
+plt.show()
+
 
 #checking the feature importance in the model
 
